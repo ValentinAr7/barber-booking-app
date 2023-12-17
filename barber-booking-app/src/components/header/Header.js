a111903ec414b2737b06168048ee2f7186eb474e
ValentinAr7 <valio990210@gmail.com>
2023-12-17 11:25:35 +0200

    [Barber-App] Header Section with styling

---
+++ barber-booking-app/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./header.module.css";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Faded Barbers</h2>
+
+      <IconButton className={styles.icon}>
+        <MenuIcon />
+      </IconButton>
+    </div>
+  );
+};
+
+export default Header;
