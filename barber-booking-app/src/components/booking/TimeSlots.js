0209ae797fa70702ab7c11d90a2cef2ed62132cb
ValentinAr7 <valio990210@gmail.com>
2023-12-22 17:41:03 +0200

    [Barber-App] Select time slots logic and style

--- barber-booking-app/src/components/booking/TimeSlots.js
+++ barber-booking-app/src/components/booking/TimeSlots.js
@@ -1,6 +1,9 @@
 import styles from "./booking.module.css";
+import { useState } from "react";
 
 const TimeSlots = () => {
+  const [selectedSlot, setSelectedSlot] = useState(null);
+
   let startDate = new Date();
   startDate.setHours(9, 0, 0);
 
@@ -17,11 +20,22 @@ const TimeSlots = () => {
     currentTime.setMinutes(currentTime.getMinutes() + 45);
   }
 
+  const handleSelectedSlot = (index) => {
+    setSelectedSlot(index);
+  };
+
   return (
     <div className={styles.mainContainer}>
+      <h3>Available Hours</h3>
       <div className={styles.slotsContainer}>
         {timeSlots.map((slot, index) => (
-          <div key={index} className={styles.slots}>
+          <div
+            key={index}
+            className={`${styles.slots} ${
+              selectedSlot === index ? styles.selectedSlot : ""
+            }`}
+            onClick={() => handleSelectedSlot(index)}
+          >
             {slot}
           </div>
         ))}
