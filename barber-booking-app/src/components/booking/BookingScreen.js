18d7e07db2a0b6e5a7f156696eb75e7eaf879c7c
ValentinAr7 <valio990210@gmail.com>
2023-12-23 16:32:16 +0200

    [Barber-App] Calendar default value and starting date set

--- barber-booking-app/src/components/booking/BookingScreen.js
+++ barber-booking-app/src/components/booking/BookingScreen.js
@@ -2,14 +2,21 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 import styles from "./booking.module.css";
+import dayjs from "dayjs";
 
 const BookingScreen = () => {
+  const minDate = dayjs();
   return (
     <>
       <h3>Book a date</h3>
       <div className={styles.calendarContainer}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <StaticDatePicker className={styles.calendar} orientation="StaticDatePicker" />
+          <StaticDatePicker
+            className={styles.calendar}
+            orientation="StaticDatePicker"
+            minDate={minDate}
+            value={minDate}
+          />
         </LocalizationProvider>{" "}
       </div>
     </>
