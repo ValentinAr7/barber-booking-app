626f7de3d06899ea87177a19bcd5e6cebb1bb778
ValentinAr7 <valio990210@gmail.com>
2023-12-23 16:53:51 +0200

    [Barber-App] useNavigation for returns

--- barber-booking-app/src/components/header/Header.js
+++ barber-booking-app/src/components/header/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./header.module.css";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={styles.container}>
+      <IconButton className={styles.arrow} onClick={handleGoBack}>
+        <ArrowBackIcon />
+      </IconButton>
       <h2 className={styles.header}>Faded Barbers</h2>
       <IconButton className={styles.icon}>
         <MenuIcon />
